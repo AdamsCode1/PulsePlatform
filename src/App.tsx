3f275d886e33c9422f66c69f4d5a8f84c88eaa84
Jakub <jakubnosek72@gmail.com>
2025-07-18 00:29:26 +0100

    Started login system for students and societies

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import EventSubmissionPage from "./pages/EventSubmissionPage";
+import SocietyLogin from "./pages/SocietyLogin";
+import UserTypeSelection from "./pages/UserTypeSelection";
+import SocietyRegister from "./pages/SocietyRegister";
+import StudentLogin from "./pages/StudentLogin";
+import StudentRegister from "./pages/StudentRegister";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +23,11 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/submit-event" element={<EventSubmissionPage />} />
+          <Route path="/login" element={<UserTypeSelection />} />
+          <Route path="/login/society" element={<SocietyLogin />} />
+          <Route path="/register/society" element={<SocietyRegister />} />
+          <Route path="/login/student" element={<StudentLogin />} />
+          <Route path="/register/student" element={<StudentRegister />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
